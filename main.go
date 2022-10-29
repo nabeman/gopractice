11263b8325360de4dced8e9c501b4dab3384c660
nabeman <tkmcoins20@gmail.com>
2022-10-29 14:54:21 +0900

    htmlファイルを返せるようにした

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 func main() {
 	http.HandleFunc("/foo", fooHandleFunc)
 
+	fs := http.FileServer(http.Dir("templates"))
+	http.Handle("/", fs)
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
@@ -18,3 +21,4 @@ func main() {
 func fooHandleFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "/fooページです")
 }
+
